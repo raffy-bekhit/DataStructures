7ea4d6312a70be2cefbc05c0a8a3db76cf8e0ad2
rafraf96 <raffybekhit@gmail.com>
2016-08-24 16:23:05 +0200

    add removeEdge function
    implement removing edges

--- Graph.cpp
+++ Graph.cpp
@@ -37,6 +37,15 @@ public:
         }
         else printf("One of the nodes doesn't exist.\n");
     }
+//remove edge 
+    void removeEdge(int node1,int node2){
+     if(node1>=0&&node2>=0&&node1<numberOfVertix&&node2<numberOfVertix)
+        {
 
+            adjMatrix[node1][node2]=false;
+            adjMatrix[node2][node1]=false;
+        }
+        else printf("One of the nodes doesn't exist.\n");
+    }
 };
 
